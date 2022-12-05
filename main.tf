8a81d2a12dd6309e8bc2243fb620bf685461fa74
Sugandhakmi <119839334+Sugandhakmi@users.noreply.github.com>
2022-12-05 02:25:23 -0600

    pass values from vars

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.pub_subnet1_CIDR
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.az_a
   map_public_ip_on_launch = true
 
   tags = {
@@ -25,7 +25,7 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.pub_subnet2_CIDR
-  availability_zone       = "us-east-1b"
+  availability_zone       = var.az_b
   map_public_ip_on_launch = true
 
   tags = {
@@ -37,7 +37,7 @@ resource "aws_subnet" "public-subnet-2" {
 resource "aws_subnet" "app-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.app_subnet1
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.az_a
   map_public_ip_on_launch = false
 
   tags = {
@@ -48,7 +48,7 @@ resource "aws_subnet" "app-subnet-1" {
 resource "aws_subnet" "app-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.app_subnet2
-  availability_zone       = "us-east-1b"
+  availability_zone       = var.az_b
   map_public_ip_on_launch = false
 
   tags = {
@@ -60,7 +60,7 @@ resource "aws_subnet" "app-subnet-2" {
 resource "aws_subnet" "db-subnet-1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.db_subnet1_CIDR
-  availability_zone = "us-east-1a"
+  availability_zone = var.az_a
 
   tags = {
     Name = "Database Subnet-1"
@@ -70,7 +70,7 @@ resource "aws_subnet" "db-subnet-1" {
 resource "aws_subnet" "db-subnet-2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.db_subnet2_CIDR
-  availability_zone = "us-east-1b"
+  availability_zone = var.az_b
 
   tags = {
     Name = "Database Subnet-2"
@@ -90,7 +90,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.vpc_cidr
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -217,7 +217,7 @@ resource "aws_lb" "alb" {
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
 
-resource "aws_lb_target_group" "alb" {
+resource "aws_lb_target_group" "target-alb" {
   name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
@@ -225,14 +225,14 @@ resource "aws_lb_target_group" "alb" {
 }
 
 resource "aws_lb_target_group_attachment" "alb1" {
-  target_group_arn = aws_lb_target_group.alb.arn
+  target_group_arn = aws_lb_target_group.target-alb.arn
   target_id        = aws_instance.web1.id
   port             = 80
   depends_on       = [aws_instance.web1]
 }
 
 resource "aws_lb_target_group_attachment" "alb2" {
-  target_group_arn = aws_lb_target_group.alb.arn
+  target_group_arn = aws_lb_target_group.target-alb.arn
   target_id        = aws_instance.web2.id
   port             = 80
   depends_on       = [aws_instance.web2]
@@ -245,7 +245,7 @@ resource "aws_lb_listener" "alb" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb.arn
+    target_group_arn = aws_lb_target_group.target-alb.arn
   }
 }
 
@@ -263,7 +263,7 @@ resource "aws_db_instance" "database" {
 }
 
 resource "aws_db_subnet_group" "default" {
-  name       = "main"
+  name       = "db-subnet-group"
   subnet_ids = [aws_subnet.db-subnet-1.id, aws_subnet.db-subnet-2.id]
 
   tags = {
